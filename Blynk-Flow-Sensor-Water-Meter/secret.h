e1f6a89b6a14e2ac24e7df3d8e7e95f29870c9d7
spillamatt <56751457+spillamatt@users.noreply.github.com>
2022-05-23 10:31:14 +0800

    Update secret.h

--- Blynk-Flow-Sensor-Water-Meter/secret.h
+++ Blynk-Flow-Sensor-Water-Meter/secret.h
@@ -7,19 +7,19 @@
          Blynk Settings
  **************************************************/
 
-#define AUTH "Your Blynk Auth Token"                            // You should get Auth Token in the Blynk App.  
+#define AUTH "hzmKRvTNXKA1Zbj38V9281ViGl-NX-VS"                            // You should get Auth Token in the Blynk App.  
 
 /***************************************************
           WiFi Settings
  **************************************************/
 
-#define WIFI_SSID "Your WiFi Network Name"                                    
-#define WIFI_PASS "Your Wifi Password"                                   
+#define WIFI_SSID "belkin.227"                                    
+#define WIFI_PASS "4a28664f"                                   
 
 /***************************************************
           Server Settings
  **************************************************/
 
-#define SERVER "xxx.xxx.xx.xxx"                                 // Comment-out if use Blynk hosted cloud service
+//#define SERVER "xxx.xxx.xx.xxx"                                 // Comment-out if use Blynk hosted cloud service
 
-#define PORT 8080
+//#define PORT 8080
